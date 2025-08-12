5a22e9a87ac37dab896ad8c0e53034fd2f258605
HuzaifaAhmed-07 <huzaifaaahmed164@gmail.com>
2025-08-12 09:56:42 -0700

    Implemented the Home feed

--- Instagram CLone/Home_Feed.h
+++ Instagram CLone/Home_Feed.h
@@ -1,7 +1,11 @@
 #pragma once
-
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <msclr\marshal_cppstd.h>
+using namespace std;
+using namespace msclr::interop;
 namespace InstagramCLone {
-
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -14,10 +18,14 @@ namespace InstagramCLone {
 	/// </summary>
 	public ref class Home_Feed : public System::Windows::Forms::Form
 	{
+		void OnFormLoad(System::Object^ sender, System::EventArgs^ e) {
+			LoadDummyPosts();
+		}
 	public:
 		Home_Feed(void)
 		{
 			InitializeComponent();
+			this->Load += gcnew System::EventHandler(this, &InstagramCLone::Home_Feed::OnFormLoad);
 			//
 			//TODO: Add the constructor code here
 			//
@@ -60,21 +68,78 @@ namespace InstagramCLone {
 			this->flowfeed->FlowDirection = System::Windows::Forms::FlowDirection::TopDown;
 			this->flowfeed->Location = System::Drawing::Point(0, 0);
 			this->flowfeed->Name = L"flowfeed";
-			this->flowfeed->Size = System::Drawing::Size(418, 724);
+			this->flowfeed->Size = System::Drawing::Size(438, 724);
 			this->flowfeed->TabIndex = 0;
 			this->flowfeed->WrapContents = false;
+			this->flowfeed->HorizontalScroll->Enabled = false;
+			this->flowfeed->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Home_Feed::flowfeed_Paint);
 			// 
 			// Home_Feed
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(418, 724);
+			this->ClientSize = System::Drawing::Size(438, 724);
 			this->Controls->Add(this->flowfeed);
+			this->MaximumSize = System::Drawing::Size(460, 780);
 			this->Name = L"Home_Feed";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Home_Feed";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void flowfeed_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+	public: 
+        void LoadDummyPosts() {
+            std::string username, caption, path, line;
+            std::ifstream ifile("posts.txt");
+            while (getline(ifile, line))
+            {
+                stringstream ss(line);
+                getline(ss, username, '|');
+                getline(ss, path, '|');
+                getline(ss, caption);
+
+                Panel^ postPanel = gcnew Panel();
+                postPanel->Size = Drawing::Size(400, 350);
+                postPanel->BorderStyle = BorderStyle::FixedSingle;
+                postPanel->Margin = Windows::Forms::Padding(10);
+                postPanel->Width = this->flowfeed->ClientSize.Width - 40;
+
+                // Username label
+                Label^ usernameLabel = gcnew Label();
+                usernameLabel->Text = marshal_as<String^>(username);
+                usernameLabel->Font = gcnew Drawing::Font("Segoe UI", 10, FontStyle::Bold);
+                usernameLabel->Dock = DockStyle::Top;
+                usernameLabel->Height = 30;
+                usernameLabel->TextAlign = ContentAlignment::MiddleLeft;
+
+                // Image
+                PictureBox^ imageBox = gcnew PictureBox();
+                imageBox->Image = Image::FromFile(marshal_as<String^>(path)); 
+                imageBox->SizeMode = PictureBoxSizeMode::Zoom;
+                imageBox->Height = 250;
+                imageBox->Dock = DockStyle::Top;
+
+                // Caption label
+                Label^ captionLabel = gcnew Label();
+                captionLabel->Text = marshal_as<String^>(caption); 
+                captionLabel->Dock = DockStyle::Bottom;
+                captionLabel->Height = 50;
+                captionLabel->Font = gcnew Drawing::Font("Segoe UI", 9);
+                captionLabel->Padding = Windows::Forms::Padding(5);
+                captionLabel->TextAlign = ContentAlignment::TopLeft;
+
+                // Add to panel
+                postPanel->Controls->Add(captionLabel);
+                postPanel->Controls->Add(imageBox);
+                postPanel->Controls->Add(usernameLabel);
+
+                // Add to feed
+                flowfeed->Controls->Add(postPanel);
+            }
+        }
+
 	};
 }
